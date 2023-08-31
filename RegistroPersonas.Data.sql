731402f051610b3fed07454d54d934b96c1efb5e
marlong03 <marlongyes@gmail.com>
2023-08-30 23:19:49 -0500

    Agrego script de base de datos completa

---
+++ RegistroPersonas.Data.sql
@@ -0,0 +1,138 @@
+USE [master]
+GO
+/****** Object:  Database [RegistroPersonas.Data]    Script Date: 30/08/2023 11:18:03 p. m. ******/
+CREATE DATABASE [RegistroPersonas.Data]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'RegistroPersonas.Data', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\RegistroPersonas.Data.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'RegistroPersonas.Data_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\RegistroPersonas.Data_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [RegistroPersonas.Data].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET READ_COMMITTED_SNAPSHOT ON 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET RECOVERY FULL 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET  MULTI_USER 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'RegistroPersonas.Data', N'ON'
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [RegistroPersonas.Data]
+GO
+/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 30/08/2023 11:18:04 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[__EFMigrationsHistory](
+	[MigrationId] [nvarchar](150) NOT NULL,
+	[ProductVersion] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY CLUSTERED 
+(
+	[MigrationId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Persona]    Script Date: 30/08/2023 11:18:04 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Persona](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](max) NOT NULL,
+	[sexo] [nvarchar](1) NOT NULL,
+	[nacimiento] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Persona] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20230830232102_initial', N'6.0.21')
+GO
+INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20230831002147_initial2', N'6.0.21')
+GO
+SET IDENTITY_INSERT [dbo].[Persona] ON 
+GO
+INSERT [dbo].[Persona] ([id], [nombre], [sexo], [nacimiento]) VALUES (1, N'Malong Mendoza', N'M', CAST(N'2003-11-09T20:08:00.0000000' AS DateTime2))
+GO
+INSERT [dbo].[Persona] ([id], [nombre], [sexo], [nacimiento]) VALUES (2, N'Alex Becerra', N'M', CAST(N'2008-06-21T21:24:00.0000000' AS DateTime2))
+GO
+INSERT [dbo].[Persona] ([id], [nombre], [sexo], [nacimiento]) VALUES (3, N'Rosa', N'F', CAST(N'1998-02-13T21:25:00.0000000' AS DateTime2))
+GO
+INSERT [dbo].[Persona] ([id], [nombre], [sexo], [nacimiento]) VALUES (4, N'Stiven', N'M', CAST(N'1990-03-30T21:26:00.0000000' AS DateTime2))
+GO
+INSERT [dbo].[Persona] ([id], [nombre], [sexo], [nacimiento]) VALUES (5, N'Juana Valentina', N'F', CAST(N'2023-08-31T21:43:00.0000000' AS DateTime2))
+GO
+SET IDENTITY_INSERT [dbo].[Persona] OFF
+GO
+USE [master]
+GO
+ALTER DATABASE [RegistroPersonas.Data] SET  READ_WRITE 
+GO
